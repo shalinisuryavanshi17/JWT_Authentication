65de17be0fb41f178a493178c3eb2109b2983aa8
G <gajbinakr.shalini0@gmail.com>
2021-05-15 10:26:13 +0530

    jwt created

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
 
 const handleErrors = (err) => {
   let errors = { email: "", password: "" };
@@ -18,6 +19,12 @@ const handleErrors = (err) => {
   }
   return errors;
 };
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, "this is my secret", {
+    expiresIn: maxAge,
+  });
+};
 module.exports.singup_get = (req, res) => {
   res.render("signup");
 };
@@ -28,10 +35,12 @@ module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie('jwt',token,{httpOnly:true,maxAge:1000*maxAge})
+    res.status(201).json({ user:user._id });
   } catch (error) {
-   const errors= handleErrors(error);
-    res.status(400).json({errors});
+    const errors = handleErrors(error);
+    res.status(400).json({ errors });
   }
 };
 module.exports.login_post = (req, res) => {
